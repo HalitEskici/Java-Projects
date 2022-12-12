c1dd5988adb01c1c3bdc83246f2c393763ceeb6e
Halit Eskici <88577329+HalitEskici@users.noreply.github.com>
2022-12-12 20:15:30 +0300

    Create Teacher.java

---
+++ java-projects/Student Information System/Teacher.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Teacher {
+    String name;
+    String phoneNumber;
+    String branch;
+
+    Teacher(String name, String phoneNumber, String branch) {
+        this.name = name;
+        this.branch = branch;
+        this.phoneNumber = phoneNumber;
+    }
+}
